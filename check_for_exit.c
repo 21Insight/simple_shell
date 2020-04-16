c8478e98e83c692f0272fe2de0009a7c19e90c37
Antonio Estela <antonioestela73@gmail.com>
2020-04-16 18:55:29 +0000

    [Project development]: Added all files

---
+++ check_for_exit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * check_exit - function that exits the program.
+ * @args: pointer that contains all the arguments of the program.
+ * @buffer: string that contains all the arguments taked by getline.
+ * @exit_value: number to exit.
+ * Return: Nothing, it is a void function :D
+ */
+void check_exit(char **args, char *buffer, int exit_value)
+{
+	if (_strncmp(args[0], "exit") == 0 && args[1] != NULL)
+	{
+		exit_value = _atoi(args[1]);
+		free_doubleptr(args);
+		free(buffer);
+		exit(exit_value);
+	}
+	else
+	{
+		free_doubleptr(args);
+		free(buffer);
+		exit(exit_value);
+	}
+}
